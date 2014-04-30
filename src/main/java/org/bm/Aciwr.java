3a4042e2eed25ff2952c416b859de2a7a0369298
rrv <admin@mail.ru>
2014-04-30 21:05:19 +0400

    aci world russia

---
+++ src/main/java/org/bm/Aciwr.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.bm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Black Moon
+ *
+ */
+public class Aciwr {
+	private final static int CAPACITY = 100;
+	
+	static final Logger log = Logger.getLogger(Aciwr.class);
+	 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List<String> words = new ArrayList<String>(CAPACITY);
+		
+		log.info("Start");
+	}
+
+}
