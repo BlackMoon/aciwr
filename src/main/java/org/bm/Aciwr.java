51147ed48d6594f48e49fc61c29202794f05a8ea
rrv <admin@mail.ru>
2014-05-01 23:18:36 +0400

    Collections.min

--- src/main/java/org/bm/Aciwr.java
+++ src/main/java/org/bm/Aciwr.java
@@ -3,8 +3,12 @@
  */
 package org.bm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 
@@ -13,17 +17,63 @@ import org.apache.log4j.Logger;
  *
  */
 public class Aciwr {
-	private final static int CAPACITY = 100;
-	
 	static final Logger log = Logger.getLogger(Aciwr.class);
 	 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<String> words = new ArrayList<String>(CAPACITY);
-		
 		log.info("Start");
+		
+		Processor p = new Processor();
+		
+		Collection<String> words = p.GenerateWords();		
+		Iterator<String> it = words.iterator();
+		
+		String word;
+		Integer cnt, min = 0;
+		
+		Map<String, Integer> entries = new HashMap<String, Integer>(),			// word entries
+							 sorted = new HashMap<String, Integer>();
+		
+		while(it.hasNext()){			
+			word = it.next();
+			
+			cnt = new Integer(1);
+			if (entries.containsKey(word))
+				cnt = entries.get(word) + 1;
+			
+			entries.put(word, cnt);				
+			
+			// fill sorted Map
+			if (sorted.size() < p.FrequentSize){
+				sorted.put(word, cnt);
+				
+				min = Collections.min(sorted.values());
+			}
+			else {
+				if (cnt > min) {
+					sorted = MapUtil.sortByValue(sorted);
+					
+					if (!sorted.containsKey(word))
+					{	
+						// remove first entry
+						Iterator<Entry<String, Integer>> it_first = sorted.entrySet().iterator();
+						it_first.next();
+						it_first.remove(); 
+					}
+					
+					sorted.put(word, cnt);
+					min = Collections.min(sorted.values());
+				}
+			}			
+		}
+		
+		for (Entry<String, Integer> e: MapUtil.sortByValue(sorted).entrySet()){
+			System.out.println(e.getKey() + " - " + e.getValue());
+		}
+		
+		System.exit(0);
 	}
 
 }
